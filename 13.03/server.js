6e3fa1b83b4bb7fde82948c7f5b700a3bf2e54b0
Juan <juanlambari1201@gmail.com>
2025-04-02 15:08:33 -0300

    JUAN

--- 13.03/server.js
+++ 13.03/server.js
@@ -2,47 +2,41 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const db = require('./13.03/banco');
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Endpoint para cadastrar cliente
-app.post('/api/motorista', (req, res) => {
-    const cliente = req.body;
-    console.log('Cliente cadastrado:', cliente);
-    res.status(201).send('Cliente cadastrado com sucesso!');
+// Rota para cadastrar motorista
+app.post('/cadastrar', (req, res) => {
+    const { nome, idade, cnh } = req.body;
+    const stmt = db.prepare("INSERT INTO motorista (nome_motorista, cpf) VALUES (?, ?)");
+    stmt.run(nome_motorista, cpf, function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ id: this.lastID, nome_motorista, cpf});
+    });
+    stmt.finalize();
 });
 
-// Endpoint para cadastrar estabelecimento
-app.post('/api/empresa', (req, res) => {
-    const estabelecimento = req.body;
-    console.log('Estabelecimento cadastrado:', estabelecimento);
-    res.status(201).send('Estabelecimento cadastrado com sucesso!');
+// Rota para listar motoristas
+app.get('/motorista', (req, res) => {
+    db.all("SELECT * FROM motorista", [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
 });
 
-// Endpoint para cadastrar entregador
-/*app.post('/api/entregador', (req, res) => {
-    const entregador = req.body;
-    console.log('Entregador cadastrado:', entregador);
-    res.status(201).send('Entregador cadastrado com sucesso!');
-});
-*/
-
-// Endpoint para cadastrar endereço
-app.post('/api/endereco', (req, res) => {
-    const endereco = req.body;
-    console.log('Endereço cadastrado:', endereco);
-    res.status(201).send('Endereço cadastrado com sucesso!');
-});
-
+// Iniciar o servidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
 // Selecione o botão de enviar currículo
 const enviarCurriculoButton = document.getElementById('enviar-curriculo');
-
