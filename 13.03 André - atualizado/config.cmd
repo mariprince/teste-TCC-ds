5d10ed6dbad2c014e175b21a5b13f7e2c40b44c9
mariprince <marinadasilvadasilvana@gmail.com>
2025-09-18 13:59:48 -0300

    se Deus quiser deu certo

--- 13.03 André - atualizado/config.cmd
+++
@@ -1,2 +0,0 @@
-git config --global user.name "mariprince"
-git config --global user.email "marinadasilvadasilvana@gmail.com"
--- 13.03 André - atualizado/empresa.controller.php
+++
@@ -1,3 +0,0 @@
-<?php 
-    require_once 'controller/empresa.controller.php';
-?>
